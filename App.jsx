9471d89ea21a6888bddad79fa97d3b967a173ce6
Chart1800 <cameronh11@usf.edu>
2024-07-09 20:18:10 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+// U94741303
+
+import React from 'react';
+
+const MeetTheTeamPage= () => { 
+    return (
+   <Header> <h1>Meet the Dream Team!</h1></Header>
+);
+}
+
+export default MeetTheTeamPage;
+
+
---
+++ Gallery1.jsx
@@ -0,0 +1,22 @@
+// U94741303
+
+function MeetTheTeamPage({Name, Photo, Role}){
+  return(
+  <div> className='MeetTheTeamPage'
+    <h2>Mr.Clean</h2>
+  <p>Operations Manager</p>
+  <img src= 'https://upload.wikimedia.org/wikipedia/en/thumb/7/73/Mr._Clean_logo.png/220px-Mr._Clean_logo.png' />
+ 
+ <h3>Rob Gronkowski</h3>
+ <p>Chief Technology Officer</p>
+ <img src= 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/13229.png' />
+
+<h4>Ronald Mcdonald </h4>
+<p> Chief Finicial Officer (CFO)</p>
+<img src= 'https://static.wikia.nocookie.net/ronaldmcdonald/images/0/0a/Original2.jpg/revision/latest?cb=20180909053442' />
+ 
+  </div>
+);
+  }
+  
+  export default MeetTheTeamPage;
